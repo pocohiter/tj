095758f9eb2d5ae24cf3d881433a7de1b6cdcb9c
pocohiter <101243568+pocohiter@users.noreply.github.com>
2022-03-09 18:26:43 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,23 +15,75 @@ cat << EOF > /etc/xray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
+                        "id": "", // 填写你的 UUID
+                        "flow": "xtls-rprx-direct",
+                        "level": 0,
+                        "email": "love@example.com"
                     }
                 ],
                 "decryption": "none",
                 "fallbacks": [
                     {
-                        "dest": 80
+                        "dest": 1310, // 默认回落到 Xray 的 Trojan 协议
+                        "xver": 1
                     },
                     {
-                        "path": "/", // 必须换成自定义的 PATH
+                        "path": "/websocket", // 必须换成自定义的 PATH
                         "dest": 1234,
+                        "xver": 1
+                    },
+                    {
+                        "path": "/vmesstcp", // 必须换成自定义的 PATH
+                        "dest": 2345,
+                        "xver": 1
+                    },
+                    {
+                        "path": "/vmessws", // 必须换成自定义的 PATH
+                        "dest": 3456,
                         "xver": 1
                     }
                 ]
             },
             "streamSettings": {
                 "network": "tcp",
+                "security": "xtls",
+                "xtlsSettings": {
+                    "alpn": [
+                        "http/1.1"
+                    ],
+                    "certificates": [
+                        {
+                            "certificateFile": "/path/to/fullchain.crt", // 换成你的证书，绝对路径
+                            "keyFile": "/path/to/private.key" // 换成你的私钥，绝对路径
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            "port": 1310,
+            "listen": "127.0.0.1",
+            "protocol": "trojan",
+            "settings": {
+                "clients": [
+                    {
+                        "password": "", // 填写你的密码
+                        "level": 0,
+                        "email": "love@example.com"
+                    }
+                ],
+                "fallbacks": [
+                    {
+                        "dest": 80 // 或者回落到其它也防探测的代理
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "none",
+                "tcpSettings": {
+                    "acceptProxyProtocol": true
+                }
             }
         },
         {
@@ -41,7 +93,9 @@ cat << EOF > /etc/xray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
+                        "id": "", // 填写你的 UUID
+                        "level": 0,
+                        "email": "love@example.com"
                     }
                 ],
                 "decryption": "none"
@@ -51,7 +105,58 @@ cat << EOF > /etc/xray/config.json
                 "security": "none",
                 "wsSettings": {
                     "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
-                    "path": "/" // 必须换成自定义的 PATH，需要和上面的一致
+                    "path": "/websocket" // 必须换成自定义的 PATH，需要和分流的一致
+                }
+            }
+        },
+        {
+            "port": 2345,
+            "listen": "127.0.0.1",
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "", // 填写你的 UUID
+                        "level": 0,
+                        "email": "love@example.com"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "none",
+                "tcpSettings": {
+                    "acceptProxyProtocol": true,
+                    "header": {
+                        "type": "http",
+                        "request": {
+                            "path": [
+                                "/vmesstcp" // 必须换成自定义的 PATH，需要和分流的一致
+                            ]
+                        }
+                    }
+                }
+            }
+        },
+        {
+            "port": 3456,
+            "listen": "127.0.0.1",
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "", // 填写你的 UUID
+                        "level": 0,
+                        "email": "love@example.com"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
+                    "path": "/vmessws" // 必须换成自定义的 PATH，需要和分流的一致
                 }
             }
         }
