0d2025fd23b4f33a90f6a666df8656d8119099f0
pocohiter <101243568+pocohiter@users.noreply.github.com>
2022-03-09 18:55:41 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,7 @@ cat << EOF > /etc/xray/config.json
                 "clients": [
                     {
                         "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
-                        "flow": "xtls-rprx-direct",
-                        "level": 0,
-                        "email": "love@example.com"
+                        "flow": "xtls-rprx-direct"
                     }
                 ],
                 "decryption": "none",
@@ -28,21 +26,62 @@ cat << EOF > /etc/xray/config.json
                         "xver": 1
                     },
                     {
-                        "path": "/websocket", // 必须换成自定义的 PATH
                         "dest": 1234,
                         "xver": 1
                     },
                     {
-                        "path": "/vmesstcp", // 必须换成自定义的 PATH
                         "dest": 2345,
                         "xver": 1
                     },
                     {
-                        "path": "/vmessws", // 必须换成自定义的 PATH
                         "dest": 3456,
                         "xver": 1
                     }
                 ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
+                }
+            }
+        },
+        {
+            "port": 1310,
+            "listen": "127.0.0.1",
+            "protocol": "trojan",
+            "settings": {
+                "clients": [
+                    {
+                        "password": "e40d2888-03f6-4859-e84d-a743db763d52" // 填写你的密码
+                    }
+                ],
+                "fallbacks": [
+                    {
+                        "dest": 80 // 或者回落到其它也防探测的代理
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "acceptProxyProtocol": true
+                }
+            }
+        },
+        {
+            "port": 1234,
+            "listen": "127.0.0.1",
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "e40d2888-03f6-4859-e84d-a743db763d52" // 填写你的 UUID
+                    }
+                ],
+                "decryption": "none"
             },
             "streamSettings": {
                 "network": "tcp",
@@ -61,64 +100,13 @@ cat << EOF > /etc/xray/config.json
             }
         },
         {
-            "port": 1310,
-            "listen": "127.0.0.1",
-            "protocol": "trojan",
-            "settings": {
-                "clients": [
-                    {
-                        "password": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的密码
-                        "level": 0,
-                        "email": "love@example.com"
-                    }
-                ],
-                "fallbacks": [
-                    {
-                        "dest": 80 // 或者回落到其它也防探测的代理
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "tcp",
-                "security": "none",
-                "tcpSettings": {
-                    "acceptProxyProtocol": true
-                }
-            }
-        },
-        {
-            "port": 1234,
-            "listen": "127.0.0.1",
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
-                        "level": 0,
-                        "email": "love@example.com"
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws",
-                "security": "none",
-                "wsSettings": {
-                    "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
-                    "path": "/websocket" // 必须换成自定义的 PATH，需要和分流的一致
-                }
-            }
-        },
-        {
             "port": 2345,
             "listen": "127.0.0.1",
             "protocol": "vmess",
             "settings": {
                 "clients": [
                     {
-                        "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
-                        "level": 0,
-                        "email": "love@example.com"
+                        "id": "e40d2888-03f6-4859-e84d-a743db763d52" // 填写你的 UUID
                     }
                 ]
             },
@@ -130,8 +118,7 @@ cat << EOF > /etc/xray/config.json
                     "header": {
                         "type": "http",
                         "request": {
-                            "path": [
-                                "/vmesstcp" // 必须换成自定义的 PATH，需要和分流的一致
+                            "host": ["viettel.akamaized.net"
                             ]
                         }
                     }
@@ -145,9 +132,7 @@ cat << EOF > /etc/xray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
-                        "level": 0,
-                        "email": "love@example.com"
+                        "id": "e40d2888-03f6-4859-e84d-a743db763d52" // 填写你的 UUID
                     }
                 ]
             },
@@ -156,7 +141,6 @@ cat << EOF > /etc/xray/config.json
                 "security": "none",
                 "wsSettings": {
                     "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
-                    "path": "/vmessws" // 必须换成自定义的 PATH，需要和分流的一致
                 }
             }
         }
