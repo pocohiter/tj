4888a585d414ef4332eceebf7e64b59728dd9ae7
pocohiter <101243568+pocohiter@users.noreply.github.com>
2022-03-09 17:56:26 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,28 +5,64 @@
 rm -rf /etc/xray/config.json
 cat << EOF > /etc/xray/config.json
 {
-    "inbounds": 
-    [
+    "log": {
+        "loglevel": "warning"
+    },
+    "inbounds": [
         {
             "port": 443,
-            "protocol": "trojan",
+            "protocol": "vless",
             "settings": {
                 "clients": [
                     {
-                        "password":"e40d2888-03f6-4859-e84d-a743db763d52"
+                        "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
+                    }
+                ],
+                "decryption": "none",
+                "fallbacks": [
+                    {
+                        "dest": 80
+                    },
+                    {
+                        "path": "/", // 必须换成自定义的 PATH
+                        "dest": 1234,
+                        "xver": 1
                     }
                 ]
             },
             "streamSettings": {
+                "network": "tcp",
+                "security": "tls",
+                "tlsSettings": {
+                    "alpn": [
+                        "http/1.1"
+                    ]
+                }
+            }
+        },
+        {
+            "port": 1234,
+            "listen": "127.0.0.1",
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
                 "network": "ws",
+                "security": "none",
                 "wsSettings": {
-                    "path": "/"
+                    "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
+                    "path": "/" // 必须换成自定义的 PATH，需要和上面的一致
                 }
             }
         }
     ],
-    "outbounds": 
-    [
+    "outbounds": [
         {
             "protocol": "freedom"
         }
