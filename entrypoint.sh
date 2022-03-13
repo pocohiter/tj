a10930e2719cd6b95b151a32221202b498e00437
pocohiter <101243568+pocohiter@users.noreply.github.com>
2022-03-13 18:51:53 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,46 +7,159 @@ $ cat > /etc/xray/config.json <<EOF
 {
     "inbounds": [
         {
-            "port": 10800,
-            "listen": "127.0.0.1",
-            "protocol": "socks",
-            "settings": {
-                "udp": true
-            }
-        }
-    ],
-    "outbounds": [
-        {
+            "port": 443,
             "protocol": "vless",
             "settings": {
-                "vnext": [
+                "clients": [
                     {
-                        "address": "kings-us-02.herokuapp.com", // 换成你的域名或服务器 IP（发起请求时无需解析域名了）
-                        "port": 443,
-                        "users": [
-                            {
-                                "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
-                                "encryption": "none",
-                                "level": 0
-                            }
-                        ]
+                        "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
+                        "flow": "xtls-rprx-direct",
+                        "level": 0,
+                        "email": "nguyenhuuvuong12@gmail.com"
+                    }
+                ],
+                "decryption": "none",
+                "fallbacks": [
+                    {
+                        "dest": 1310, // 默认回落到 Xray 的 Trojan 协议
+                        "xver": 1
+                    },
+                    {
+                        "path": "/websocket", // 必须换成自定义的 PATH
+                        "dest": 1234,
+                        "xver": 1
+                    },
+                    {
+                        "path": "/vmesstcp", // 必须换成自定义的 PATH
+                        "dest": 2345,
+                        "xver": 1
+                    },
+                    {
+                        "path": "/vmessws", // 必须换成自定义的 PATH
+                        "dest": 3456,
+                        "xver": 1
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "xtls",
+                "xtlsSettings": {
+                    "alpn": [
+                        "http/1.1"
+                    ]
+                }
+            }
+        },
+        {
+            "port": 1310,
+            "listen": "127.0.0.1",
+            "protocol": "trojan",
+            "settings": {
+                "clients": [
+                    {
+                        "password": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的密码
+                        "level": 0,
+                        "email": "nguyenhuuvuong12@gmail.com"
+                    }
+                ],
+                "fallbacks": [
+                    {
+                        "dest": 80 // 或者回落到其它也防探测的代理
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "none",
+                "tcpSettings": {
+                    "acceptProxyProtocol": true
+                }
+            }
+        },
+        {
+            "port": 1234,
+            "listen": "127.0.0.1",
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
+                        "level": 0,
+                        "email": "nguyenhuuvuong12@gmail.com"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
+                    "path": "/websocket" // 必须换成自定义的 PATH，需要和分流的一致
+                }
+            }
+        },
+        {
+            "port": 2345,
+            "listen": "127.0.0.1",
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
+                        "level": 0,
+                        "email": "nguyenhuuvuong12@gmail.com"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "none",
+                "tcpSettings": {
+                    "acceptProxyProtocol": true,
+                    "header": {
+                        "type": "http",
+                        "request": {
+                            "path": [
+                                "/vmesstcp" // 必须换成自定义的 PATH，需要和分流的一致
+                            ]
+                        }
+                    }
+                }
+            }
+        },
+        {
+            "port": 3456,
+            "listen": "127.0.0.1",
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "e40d2888-03f6-4859-e84d-a743db763d52", // 填写你的 UUID
+                        "level": 0,
+                        "email": "nguyenhuuvuong12@gmail.com"
                     }
                 ]
             },
             "streamSettings": {
                 "network": "ws",
-                "security": "tls",
-                "tlsSettings": {
-                    "serverName": "viettel.akamaized.net" // 换成你的域名
+                "security": "none",
+                "wsSettings": {
+                    "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
+                    "path": "/vmessws" // 必须换成自定义的 PATH，需要和分流的一致
                 }
             }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
         }
     ]
 }
 EOF
 
 #run xray
-
-docker run -d -p 443:443 --name xray --restart=always -v /etc/xray:/etc/xray teddysun/xray
 
 xray -c /etc/xray/config.json
