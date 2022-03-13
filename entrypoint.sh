6c4642f4f205ce9a213764c49e93a6f7e599056c
pocohiter <101243568+pocohiter@users.noreply.github.com>
2022-03-13 21:12:56 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,38 +15,6 @@ $ cat > /etc/xray/config.json <<EOF
                         "id": "e40d2888-03f6-4859-e84d-a743db763d52" // 填写你的 UUID
                     }
                 ],
-                "decryption": "none",
-                "fallbacks": [
-                    {
-                        "dest": 80
-                    },
-                    {
-                        "path": "/websocket", // 必须换成自定义的 PATH
-                        "dest": 1080,
-                        "xver": 1
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "tcp",
-                "security": "tls",
-                "tlsSettings": {
-                    "alpn": [
-                        "http/1.1"
-                    ]
-                }
-            }
-        },
-        {
-            "port": 1080,
-            "listen": "127.0.0.1",
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "e40d2888-03f6-4859-e84d-a743db763d52" // 填写你的 UUID
-                    }
-                ],
                 "decryption": "none"
             },
             "streamSettings": {
@@ -54,7 +22,7 @@ $ cat > /etc/xray/config.json <<EOF
                 "security": "none",
                 "wsSettings": {
                     "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
-                    "path": "/websocket" // 必须换成自定义的 PATH，需要和上面的一致
+                    "path": "/" // 必须换成自定义的 PATH，需要和上面的一致
                 }
             }
         }
